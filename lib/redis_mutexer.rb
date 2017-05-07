dccadab48e4e2ec802eb113c0d2308b52bec3e10
Shishir Sharma <shishirsharma.in@gmail.com>
2017-05-07 20:46:13 +0530

    Using global config

--- lib/redis_mutexer.rb
+++ lib/redis_mutexer.rb
@@ -1,14 +1,17 @@
 require "redis_mutexer/version"
 require "redis"
+require 'active_support/concern'
 
 module RedisMutexer
+  extend ActiveSupport::Concern
+
   class Configuration
     attr_accessor :redis, :host, :port, :db, :time, :logger
 
     def initialize
       @host = 'localhost'    #host
       @port = 6379           #port
-      @db = 'redis_mutexer'  #database namespace
+      @db = '1'  #database namespace
       @time = 60             #seconds
     end
   end
@@ -29,7 +32,7 @@ module RedisMutexer
 
   def configure
     @config ||= Configuration.new
-    yield(config) if block_given?
+    yield(config) if block_given? 
 
     # Create a redis connection if not provided with
     @config.redis ||=
@@ -38,17 +41,21 @@ module RedisMutexer
                 db:   config.db,
                 time: config.time
                )
+    @config.logger = config.logger
+    @config.logger.debug "Config called"
+
     @config.logger ||= Logger.new(STDOUT)
   end
 
   def lock(obj, time)
-    @config.redis.setex("#{obj.class.name + ":" + obj.id.to_s}", time, self.id)
+    Logger.new(STDOUT)
+    RedisMutexer.config.redis.setex("#{obj.class.name + ":" + obj.id.to_s}", time, self.id)
   end
   def locked?(obj)
-    (@config.redis.get("#{obj.class.name + ":" + obj.id.to_s}").to_i == self.id) ? true : false
+    (RedisMutexer.config.redis.get("#{obj.class.name + ":" + obj.id.to_s}").to_i == self.id) ? true : false
   end
   def unlock(obj)
-    @config.redis.del("#{obj.class.name + ":" + obj.id.to_s}")
+    RedisMutexer.config.redis.del("#{obj.class.name + ":" + obj.id.to_s}")
   end
 
   module_function :configure
--- spec/redis_mutexer_spec.rb
+++ spec/redis_mutexer_spec.rb
@@ -1,16 +1,45 @@
 require "spec_helper"
 
 describe RedisMutexer do
-  let (:user) { User.create!(id: 1, name: "pallav sharma")}
-  let (:topic) { Topic.create!(id: 1, title: "this is test title")}
+  describe RedisMutexer::Configuration do
+    subject { RedisMutexer::Configuration.new }
+
+    attribute_list = [
+      :redis, :host, :port, :db,
+      :time, :logger
+    ]
+
+    attribute_list.each do |attr|
+      it { should respond_to attr }
+    end
+  end
 
   it "has a version number" do
     expect(RedisMutexer::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    user.lock(topic, 60)
-    user.locked?(topic)
-    user.unlock(topic)
+  subject {
+    RedisMutexer.tap do |redis_mutexer| 
+      redis_mutexer.configure  do |config|
+        config.host = "localhost"
+        config.port = "6379"
+        config.db = "1"
+        config.time = 60            # Number of api call during timeout
+        config.logger = Logger.new(STDOUT)
+      end
+    end
+  }
+
+  it { should respond_to :configure }
+
+  describe "when provided a block" do
+    let (:user) { User.create!(id: 1, name: "pallav sharma")}
+    let (:topic) { Topic.create!(id: 1, title: "this is test title")}
+
+    it "does something useful" do
+      user.lock(topic, 60)
+      user.locked?(topic)
+      user.unlock(topic)
+    end
   end
 end
