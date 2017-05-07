30b945e54546471c66b2c907a91b767c5ab7f328
IAmPallavSharma <mail.pallavsharma@gmail.com>
2017-05-07 14:59:43 +0530

    adding ActiveSupport::Concern

--- lib/redis_mutexer.rb
+++ lib/redis_mutexer.rb
@@ -2,24 +2,9 @@ require "redis_mutexer/version"
 require "redis"
 
 module RedisMutexer
-  # class Configuration
-  #   attr_accessor :host,
-  #                 :port,
-  #                 :db,
-  #                 :default_expiry_time
 
-  #   def initialize
-  #     @host = 'localhost'             #host to run redis
-  #     @port = '6379'                  #port to run redis
-  #     @db = 'redis_mutexer'           #redis database name
-  #     @default_expiry_time = 60 * 5   #seconds
-  #   end
-  # end
+  extend ActiveSupport::Concern
 
-  # class << self
-  #   attr_accessor :config
-  # end
-  
   def redis
     redis ||=
       Redis.new(:host => "localhost",
@@ -28,35 +13,14 @@ module RedisMutexer
                )
   end
 
-  # def configure
-  #   @config ||= Configuration.new
-  #   yield(config) if block_given?
-  #   # Create a redis connection if not provided with
-  #   @config.redis ||=
-  #     Redis.new(host: config.host,
-  #               port: config.port,
-  #               db: config.db
-  #              )
-  # end
-
-  class User
-    attr_accessor :id, :name, :city
-    include RedisMutexer
-
-    def lock(obj, time)
-      redis.setex("#{obj.class.name + ":" + obj.id.to_s}", time, self.id)
-    end
-    def locked?(obj)
-      (redis.get("#{obj.class.name + ":" + obj.id.to_s}").to_i == self.id) ? true : false
-    end
-    def unlock(obj)
-      redis.del("#{obj.class.name + ":" + obj.id.to_s}")
-    end
-
+  def lock(obj, time)
+    redis.setex("#{obj.class.name + ":" + obj.id.to_s}", time, self.id)
   end
-
-  def raise_assertion_error
-    raise AssertionError, 'block syntax has been removed from #lock, use #with_lock instead'
+  def locked?(obj)
+    (redis.get("#{obj.class.name + ":" + obj.id.to_s}").to_i == self.id) ? true : false
+  end
+  def unlock(obj)
+    redis.del("#{obj.class.name + ":" + obj.id.to_s}")
   end
 
   module_function :redis
